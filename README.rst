2347379a40c4b3b5be38ba7d7ef11d2858fa8a35
Krishna Madhavan <krishnamadhavan07@gmail.com>
2024-02-10 15:02:08 +0530

    feat: plugin boilerplate

---
+++ README.rst
@@ -0,0 +1,22 @@
+keeshstudio plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/krishnamadhavan/tutor-contrib-keeshstudio
+
+Usage
+-----
+
+::
+
+    tutor plugins enable keeshstudio
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,61 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutorkeeshstudio", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-contrib-keeshstudio",
+    version=ABOUT["__version__"],
+    url="https://github.com/krishnamadhavan/tutor-contrib-keeshstudio",
+    project_urls={
+        "Code": "https://github.com/krishnamadhavan/tutor-contrib-keeshstudio",
+        "Issue tracker": "https://github.com/krishnamadhavan/tutor-contrib-keeshstudio/issues",
+    },
+    license="AGPLv3",
+    author="Krishna Madhavan",
+    description="keeshstudio plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v1": [
+            "keeshstudio = tutorkeeshstudio.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
